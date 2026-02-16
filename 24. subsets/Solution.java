342d5f8029986bbc5f4ca8592f4be50bde556846
Prabhat Jana <itsmeprabhatjana@gmail.com>
2026-02-16 19:59:04 +0530

    24. Subsets

---
+++ 24. subsets/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    private void backtrack(List<List<Integer>> res, List<Integer> tempList, int[] nums, int start) {
+        res.add(new ArrayList<>(tempList));
+
+        for(int i=start; i<nums.length; i++) {
+            tempList.add(nums[i]);
+            backtrack(res, tempList, nums, i+1);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        backtrack(res, new ArrayList<>(), nums, 0);
+        return res;
+    }
+}
