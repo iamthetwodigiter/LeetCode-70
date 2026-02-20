e41655a6e13078ec8d21ce6d650c69143a9c89cc
Prabhat Jana <itsmeprabhatjana@gmail.com>
2026-02-21 00:00:42 +0530

    72. Longest Common Subsequence

---
+++ 72. longest-common-subsequence/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // dp[i][j] stores LCS of text1[0...i-1] and text2[0...j-1]
+        // We use size (m+1)x(n+1) to handle empty string base cases
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    // Match found: add 1 to the result of previous prefixes
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } else {
+                    // No match: take the best result from either skipping i or j
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
